fa8f4edd0624f77e2ba755dc7d9d2350bbe85e68
joe rizk <joerizk2@hotmail.com>
2022-01-22 23:56:13 +0200

    fixing game

--- game.js
+++ game.js
@@ -3,38 +3,61 @@ window.onload = function MazeGame() {
   var message = document.getElementById("status");
   var start_div = document.getElementById("start");
   var end_div = document.getElementById("end");
+  var restart = document.getElementsByClassName("example");
+  restart[0].innerHTML = "Restart";
+  var score = 0;
   function TurnRed() {
-    for (var i = 0; i < walls.length; i++) {
+    for (var i = 0; i < walls.length - 1; i++) {
       walls[i].style.backgroundColor = "red";
     }
-    message.innerHTML = "You Lost!😞";
+
+    if (score <= 10) {
+      score = 0;
+    } else {
+      score = score - 10;
+    }
+    message.innerHTML = `You Lost!😞 Click "S" to start again, Score = ${score}`;
   }
   function TurnGreen() {
-    for (var i = 0; i < walls.length; i++) {
+    for (var i = 0; i < walls.length - 1; i++) {
       walls[i].style.backgroundColor = "green";
     }
-    message.innerHTML = "You Won!🎉";
+    score = score + 5;
+    message.innerHTML = 'You Won!🎉 Click "S" to start again, Score = ' + score;
   }
   function TurnWhite() {
     console.log("Turn white");
-    for (var i = 0; i < walls.length; i++) {
+    for (var i = 0; i < walls.length - 1; i++) {
       walls[i].style.backgroundColor = "#eeeeee";
-      message.innerHTML = 'Begin by moving your mouse over the "S"';
     }
+    message.innerHTML = 'Begin by moving your mouse over the "S"';
   }
+  function RestartScore() {
+    for (var i = 0; i < walls.length - 1; i++) {
+      walls[i].style.backgroundColor = "#eeeeee";
+    }
+    score = 0;
+    message.innerHTML =
+      'Begin by moving your mouse over the "S", Score = ' + score;
+  }
+
   function StartAgain() {
     start_div.addEventListener("click", TurnWhite);
   }
-
   function YouLost() {
-    for (i = 0; i < walls.length; i++) {
+    for (i = 0; i < walls.length - 1; i++) {
       walls[i].addEventListener("mouseenter", TurnRed);
     }
   }
   function YouWin() {
-    end_div.addEventListener("mouseenter", TurnGreen);
+    end_div.addEventListener("click", TurnGreen);
   }
+  function RestartGame() {
+    restart[0].addEventListener("click", RestartScore);
+  }
+
   YouLost();
   StartAgain();
   YouWin();
+  RestartGame();
 };
