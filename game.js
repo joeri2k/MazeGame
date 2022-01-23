8d0df5c3d2ea05040e8b9ba243f4e4748295949f
joe rizk <joerizk2@hotmail.com>
2022-01-23 11:18:56 +0200

    Fixing game

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ window.onload = function MazeGame() {
   var end_div = document.getElementById("end");
   var restart = document.getElementsByClassName("example");
   restart[0].innerHTML = "Restart";
+  var status = "start";
   var score = 0;
   function TurnRed() {
     for (var i = 0; i < walls.length - 1; i++) {
@@ -16,7 +17,13 @@ window.onload = function MazeGame() {
     } else {
       score = score - 10;
     }
+
     message.innerHTML = `You Lost!😞 Click "S" to start again, Score = ${score}`;
+    end_div.removeEventListener("mouseenter", TurnGreen, { once: true });
+    for (i = 0; i < walls.length - 1; i++) {
+      walls[i].removeEventListener("mouseenter", TurnRed, { once: true });
+    }
+    Start();
   }
   function TurnGreen() {
     for (var i = 0; i < walls.length - 1; i++) {
@@ -24,14 +31,23 @@ window.onload = function MazeGame() {
     }
     score = score + 5;
     message.innerHTML = 'You Won!🎉 Click "S" to start again, Score = ' + score;
+    end_div.removeEventListener("mouseenter", TurnGreen, { once: true });
+    for (i = 0; i < walls.length - 1; i++) {
+      walls[i].removeEventListener("mouseenter", TurnRed, { once: true });
+    }
+    Start();
   }
   function TurnWhite() {
     console.log("Turn white");
     for (var i = 0; i < walls.length - 1; i++) {
       walls[i].style.backgroundColor = "#eeeeee";
     }
-    message.innerHTML = 'Begin by moving your mouse over the "S"';
+    message.innerHTML = 'Begin by moving your mouse over the "S".';
+    //  status = "ingame";
+    YouWin();
+    YouLost();
   }
+
   function RestartScore() {
     for (var i = 0; i < walls.length - 1; i++) {
       walls[i].style.backgroundColor = "#eeeeee";
@@ -39,25 +55,30 @@ window.onload = function MazeGame() {
     score = 0;
     message.innerHTML =
       'Begin by moving your mouse over the "S", Score = ' + score;
+    for (i = 0; i < walls.length - 1; i++) {
+      walls[i].removeEventListener("mouseenter", TurnRed, { once: true });
+    }
+    Start();
   }
 
-  function StartAgain() {
-    start_div.addEventListener("click", TurnWhite);
+  function Start() {
+    start_div.addEventListener("mouseenter", TurnWhite, { once: true });
   }
   function YouLost() {
     for (i = 0; i < walls.length - 1; i++) {
-      walls[i].addEventListener("mouseenter", TurnRed);
+      walls[i].addEventListener("mouseenter", TurnRed, { once: true });
     }
   }
   function YouWin() {
-    end_div.addEventListener("click", TurnGreen);
+    end_div.addEventListener("mouseenter", TurnGreen, { once: true });
   }
   function RestartGame() {
     restart[0].addEventListener("click", RestartScore);
   }
 
-  YouLost();
-  StartAgain();
-  YouWin();
+  if (status == "start") {
+    Start();
+    status = "ingame";
+  }
   RestartGame();
 };
